8ee6e45b87680b7e6b1205d9e35b556a7996b245
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-01-15 19:14:37 +0300

    create sendForm module

---
+++ src/components/sendForm.js
@@ -0,0 +1,79 @@
+// Отправка данных формы на сервер
+const sendForm = formName => {
+  // сообщения о статусе отправки
+	const errorMessage = 'Что-то пошло не так...';
+	const loadMessage = 'Загрузка...';
+  const successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+  // модальное окно
+  const modalWindow = document.querySelector('.modal-callback');
+
+  // форма
+  const form = document.querySelector(`#${formName}`);
+
+  // все инпуты внутри формы
+  const inputs = [
+    ...document.querySelectorAll(`#${formName}>.form-group input`),
+    ...document.querySelectorAll(`#${formName}>.form-group textarea`)];
+
+  // сообщение
+  const message = document.createElement('div');
+
+  const postData = body  => fetch('../server.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  });
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // добавляем сообщение в документ
+    form.append(message);
+    message.textContent = loadMessage;
+
+    // создаем объект Form
+    const formData = new FormData(form);
+
+    // готовим тело запроса для отправки
+    const body = {};
+    for (const [key, value] of formData) {
+      body[key] = value;
+    }
+
+    // отправляем данные на сервер
+    postData(body);
+
+    // очистка инпутов после отправки данных
+    inputs.forEach(item => item.value = '');
+
+    // удаление сообщения о статусе отправки
+    setTimeout(() => message.remove(), 5000);
+    // закрытие модального окна
+    setTimeout(() => modalWindow.style.display = 'none', 7000);
+  });
+
+  // Валидация данных
+  form.addEventListener('input', e => {
+    const target = e.target;
+    // Валидация данных при вводе телефона
+    if (target.matches('input[name="tel"]')) {
+      target.setAttribute('pattern', '^[0-9+]{11,12}$');
+      target.value = target.value.replace(/[^\d+]/g, '');
+    }
+    // Валидация данных при вводе имени
+    if (target.matches('input[name="fio"]')) {
+      target.setAttribute('pattern', '[А-Яа-яЁё-]{2,25}');
+      target.value = target.value.replace(/[^А-Яа-яЁё\s-]|/g, '');
+    }
+    // Валидация данных при вводе сообщения
+    if (target.matches('textarea[name="message"]')) {
+      target.setAttribute('type', 'text');
+      target.value = target.value.replace(/[^А-Яа-яЁё\s().,!?'";:-]/g, '');
+    }
+  });
+};
+
+export default sendForm;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import modalWindow from './components/modalWindow';
 import scroll from './components/scroll';
+import sendForm from './components/sendForm';
 
 // Модальное окно
 modalWindow();
 
 // Скролл от якорных ссылок до соответстбущих блоков
 scroll();
+
+sendForm('form-callback');
+sendForm('form-feedback');
+sendForm('form-application');
