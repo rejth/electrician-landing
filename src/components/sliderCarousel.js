bfc3f9540e8da0770c52aafab8e9185b17f31374
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-01-17 21:06:36 +0300

    finished slider-carousel

--- src/components/sliderCarousel.js
+++ src/components/sliderCarousel.js
@@ -1,85 +1,114 @@
 export default class SliderCarousel {
   constructor() {
-    this.servicesMain = document.querySelector('.our-services'); // main container (+ buttons)
+    this.servicesMain = document.querySelector('.services-elements'); // main container
     this.carouselWrapper = document.querySelector('.services-carousel'); // carousel wrapper
-    this.slides = document.querySelectorAll('.services-carousel .element'); // слайды
+    this.slides = this.carouselWrapper.children; // слайды карусели
 
     this.prev = document.querySelector('.services-arrow .arrow-left'); // кнопка назад
     this.next = document.querySelector('.services-arrow .arrow-right'); // кнопка вперед
 
-    this.slidesToShow = 3;
+    this.slidesToShow = 3; // стандартное количество слайдов на странице
 
     this.options = {
-      position: 0,
-      widthSlide: 100 / Math.floor(this.slidesToShow),
-      infinity: Infinity,
+      position: 0, // позиция слайда при пролистывании
+      widthSlide: 100 / Math.floor(this.slidesToShow), // ширина слайда в %
+      infinity: Infinity, // опция для бесконечного листания слайдов
+      // максимальный позиция слайда при пролситывания (количество кликов по кнопке)
       maxPosition: this.slides.length - this.slidesToShow
     };
 
+    // настройки для адаптива
     this.responsive = [
       {
-        breakpoint: 1024,
-        slideToShow: 3
-      },
-      {
-        breakpoint: 768,
+        breakpoint: 992,
         slideToShow: 2
       },
       {
-        breakpoint: 576,
+        breakpoint: 768,
         slideToShow: 1
       }
     ];
   }
 
+  // функция инициализации компонента
   init() {
-    this.addCustomClass();
-    this.addCustomStyle();
-    this.constrolSlider();
-    this.responsiveInit();
+    this.addCustomClass(); // добавление кастомных классов
+    this.addCustomStyle(); // добавление кастомных стилей для карусели
+    this.constrolSlider(); // переключение по кнопкам вперед и назад
+    this.responsiveInit(); // включение адаптива
   }
 
+  // функция адаптива дял карусели
   responsiveInit() {
     const slidesToShowDefault = this.slidesToShow;
-    const allResponse = this.responsive.map(item => item.breakpoint);
-    const maxResponse = Math.max(...allResponse);
+    const allResponsive = this.responsive.map(item => item.breakpoint);
+    const maxResponsive = Math.max(...allResponsive);
 
+    const checkResponse = () => {
+      const widthWindow = document.documentElement.clientWidth;
+      if (widthWindow < maxResponsive) {
+        for (let i = 0; i < allResponsive.length; i++) {
+          if (widthWindow < allResponsive[i]) {
+            this.slidesToShow = this.responsive[i].slideToShow;
+            this.options.maxPosition = this.slides.length - this.slidesToShow;
+            this.options.widthSlide = Math.floor(100 / this.slidesToShow);
+            this.addCustomStyle();
+          }
+        }
+      } else {
+        this.slidesToShow = slidesToShowDefault;
+        this.options.widthSlide = Math.floor(100 / this.slidesToShow);
+        this.addCustomStyle();
+      }
+    };
+    checkResponse();
+
+    window.addEventListener('resize', () => checkResponse());
   }
 
-
+  // функция переключения слайдов по кнопкам
   constrolSlider() {
     this.prev.addEventListener('click', () => this.prevSlide());
     this.next.addEventListener('click', () => this.nextSlide());
   }
 
+  // функция перелистывания назад
   prevSlide() {
     if (this.options.infinity || this.options.position > 0) {
       --this.options.position;
+      // автоматическая прокрутка в конец карусели, если пользователь листает назад от 0-го элемента
       if (this.options.position < 0) {
         this.options.position = this.options.maxPosition;
       }
+      // перемещение враппера вперед по оси X на -N%
       this.carouselWrapper.style.transform = `translateX(-${this.options.widthSlide * this.options.position}%)`;
     }
   }
 
+  // функция перелистывания вперед
   nextSlide() {
     if (this.options.infinity || this.options.position < this.options.maxPosition) {
       ++this.options.position;
+      // автоматическая прокрутка в начало карусели, если пользователь листает вперед от последнего элемента
       if (this.options.position > this.options.maxPosition) {
         this.options.position = 0;
       }
+      // перемещение враппера назад по оси X на -N%
       this.carouselWrapper.style.transform = `translateX(-${this.options.widthSlide * this.options.position}%)`;
     }
   }
 
+  // функция добавления классов для стилизации карусели
   addCustomClass() {
     this.servicesMain.classList.add('our-services-custom');
     this.carouselWrapper.classList.add('services-carousel-custom');
-    this.slides.forEach(item => item.classList.add('element-custom'));
+    for (const item of this.slides) {
+      item.classList.add('element-custom');
+    }
   }
 
+  // функция добавления стилей для карусели
   addCustomStyle() {
-    // создаем тэг style с id
     const style = document.createElement('style');
     style.id = 'slider-carousel-style';
     style.textContent = `
@@ -95,7 +124,7 @@ export default class SliderCarousel {
         margin: 0 0 20px 0;
       }
     `;
-
+    // добавляем тэг style в документ
     document.head.append(style);
   }
 }
